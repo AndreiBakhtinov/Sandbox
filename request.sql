03e17cfa462f0dfd2c3493aa90bb746f2f3854f2
Sergii <wheelyhog@gmail.com>
2023-05-04 11:42:41 +0200

    add new request

--- request.sql
+++ request.sql
@@ -3,3 +3,8 @@ SET price = price * 1.155
 WHERE
     CategoryID IN (1, 2)
     AND price BETWEEN 50 AND 100;
+
+SELECT
+    DISTINCT Customers.Country
+FROM Customers
+    JOIN orders ON Customers.CustomerID = Orders.CustomerID;
